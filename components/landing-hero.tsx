5a24bc370fcaa1bbcf1d0c861ea2b4361e9f650d
wdhere <wdhere@163.com>
2023-09-17 17:56:00 +0800

    fix: typo error

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -33,7 +33,7 @@ const LandingHero = () => {
         Create content using AI 10x faster.
       </div>
       <div>
-        <Link href={isSignedIn ? "/dashborad" : "/sign-up"}>
+        <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
           <Button
             variant="premium"
             className="md:text-lg p-4 md:p-6 rounded-full font-semibold"
